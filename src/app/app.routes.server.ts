edf544ca70a488a2d2b7a719fd06385ee10c15a8
unknown <hoatruongquay@gmail.com>
2025-01-06 16:07:01 +0700

    fix deploy

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,31 @@
+import { inject } from '@angular/core';
+import { BlogService } from '../services/BlogService';
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Server,  // Thay thế RenderMode bằng chuỗi 'Prerender'
+  },
+  {
+    path: 'detail/:id',
+    renderMode: RenderMode.Prerender ,  // Sử dụng prerender cho route có tham số
+    async getPrerenderParams(): Promise<Array<Record<string, string>>> {
+      const blogService = inject(BlogService);
+      const blogs = await blogService.getBlogs().toPromise();  // Lấy dữ liệu blog từ BlogService
+      // Kiểm tra blogs và tránh lỗi undefined
+      if (blogs && blogs.data) {
+        return blogs.data.map((blog: any) => ({ id: blog.id.toString() }));
+      }
+      return [];  // Nếu không có dữ liệu, trả về mảng rỗng
+    }
+  },
+  {
+    path: 'create',
+    renderMode: RenderMode.Client,  // Dùng Client-side Rendering cho route này
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Mặc định fallback sang Client-side Rendering cho các route không xác định
   }
 ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,18 @@
 import { DetailComponent } from './detail/detail.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     {
         path: 'detail/:id',
         loadComponent: () =>
-            import('./detail/detail.component').then((m) => m.DetailComponent)
+            import('./detail/detail.component').then((m) => m.DetailComponent),
     },
-    // { path: 'create', component: CreateComponent }
     {
         path: 'create',
         loadComponent: () =>
-            import('./create/create.component').then((m) => m.CreateComponent)
+            import('./create/create.component').then((m) => m.CreateComponent),
     },
 ];
 
